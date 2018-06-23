6075455a4a374b67c1519e3c216d64638745384f
Sebastian Pipping <sebastian@pipping.org>
2018-06-23 17:43:29 +0200

    Dockerize based on lazygal

---
+++ entrypoint.bash
@@ -0,0 +1,14 @@
+#! /bin/bash
+# Copyright (C) 2018 Sebastian Pipping <sebastian@pipping.org>
+# Licensed under the MIT license
+
+set -e
+
+PS4='# '
+set -x
+
+LC_ALL=C.UTF-8 lazygal /var/www/default/files/Exhibitions/
+
+if [[ $# -gt 1 ]]; then
+    "$@"
+fi
